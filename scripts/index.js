f8c58fe43c8e3f71604a7d40c6a1f3eb3989e28b
SanBorisovich <savin2201@mail.ru>
2023-12-11 19:38:44 +0300

    version 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function createCard(element) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
+  cardElement.querySelector(".card__title").textContent = element.name;
+  cardElement.querySelector(".card__image").src = element.link;
+  cardElement.querySelector(".card__image").alt = element.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener('click', function() {
+    cardElement.remove();
+  });
+
+  return cardElement;
+}
+
+// @todo: Функция удаления карточки
 // @todo: Вывести карточки на страницу
+
+function renderCards(cards) {
+  cards.forEach((item) => {
+    placesList.append(createCard(item));
+  });
+}
+
+renderCards(initialCards);
